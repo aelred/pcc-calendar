13520367c58ff8a1f0de287f444428b51bd617d0
Felix Chapman <aelred717@gmail.com>
2017-08-20 14:53:22 -0400

    Change main script to print to stdout

--- pcc.py
+++ pcc.py
@@ -4,5 +4,4 @@ from pcc.cal import make_calendar
 showings = list(scrape_showings())
 calendar = make_calendar(showings)
 
-with open('pcc-calendar.ics', 'wb') as f:
-    f.write(calendar.to_ical())
+print(calendar.to_ical().decode('utf-8'))
